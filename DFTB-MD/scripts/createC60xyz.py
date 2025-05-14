13265580b05799b7faca798f932302f8e278ec71
ossi0 <ossi.laurila000@gmail.com>
2025-05-14 13:24:00 +0300

    Update createC60xyz.py

--- DFTB-MD/scripts/createC60xyz.py
+++ DFTB-MD/scripts/createC60xyz.py
@@ -1,14 +1,9 @@
 import os
 
-C = -51.42208619083232 # Ha/Bohr to eV/Å
-
 path = os.getcwd()
 xyzfile = path.split("/")[-1].replace("cluster", "coord") + ".xyz"
 
 w = open("ref.xyz", "w")
-#w.write("60\n")
-#w.write(firstline)
-
 
 lines = open(xyzfile, "r")
 for i, val in enumerate(lines.readlines()):
