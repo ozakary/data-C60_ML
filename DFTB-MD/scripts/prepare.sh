a5c45116e98d663805ee2f669fdeba3ecc4f4483
ossi0 <ossi.laurila000@gmail.com>
2025-05-14 13:26:27 +0300

    Update prepare.sh

--- DFTB-MD/scripts/prepare.sh
+++ DFTB-MD/scripts/prepare.sh
@@ -8,9 +8,7 @@ module load turbomole
 # module load turbomole
 # module load python-env
 
-#split --additional-suffix=.xyz -d -a 4 -l 1950 xtb.trj coord_
 
-# for i in 0000; do
 for i in $(seq -f "%05g" 34000 500 200000 ); do
     mkdir cluster_${i}
     grep -B 1   iter:$i geo_end.xyz > coord_$i.xyz     
@@ -20,13 +18,8 @@ for i in $(seq -f "%05g" 34000 500 200000 ); do
     cd ./cluster_${i}
     python3 createC60xyz.py
     rm -f coord_$i.xyz
-    mv ref.xyz coord_$i.xyz
+    mv temp.xyz coord_$i.xyz
     x2t coord_${i}.xyz > coord
-    cp ../define_input .
-    cp ../turbomole_carpo2.job .
-    define < define_input
-    sbatch turbomole_carpo2.job
     cd ..
 done
 
-# ./run_turbomole.sh
