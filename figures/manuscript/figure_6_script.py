00b382373ef162590b8b7e81f2b7f3fab4a8d94f
ossi0 <ossi.laurila000@gmail.com>
2025-07-15 14:03:32 +0300

    Update figure_6_script.py

--- figures/manuscript/figure_6_script.py
+++ figures/manuscript/figure_6_script.py
@@ -24,9 +24,9 @@ linestyle_tuple = [
 #Load data of simulation in folder f and split the data at interval s
 def ld(f, s):
     try:
-        return np.concatenate(np.loadtxt('./' + f + '/allvol.txt')[::s], axis=None)
+        return np.concatenate(np.loadtxt('./' + f + '/volumes_ell_average.txt')[0::s], axis=None)
     except FileNotFoundError:
-        return np.concatenate(np.loadtxt('./' + f + '/volumes_trace.txt')[0::s], axis=None)
+        return np.concatenate(np.loadtxt('./' + f + '/allvol.txt')[0::s], axis=None)
 
 #Calculate mean and stdev and standard error
 def calc_stats(data):
@@ -40,34 +40,34 @@ def calc_stats(data):
     return means, devs, sems
 
 # Load the data
-volumes_1bead = [ld('100K/1b', 1), ld('150K/1b', 1), ld('200K/1b', 1), ld('250K/1b', 1), 
-                ld('300K/1b', 1), ld('350K/1b', 1), ld('400K/1b', 1), ld('500K/1b', 1)]
+volumes_1bead = [ld('100K/100K_1b', 1), ld('150K/150K_1b', 1), ld('200K/200K_1b', 1), ld('250K/250K_1b', 1), 
+                ld('300K/300K_1b', 1), ld('350K/350K_1b', 1), ld('400K/400K_1b', 1), ld('500K/500K_1b', 1)]
 
-volumes_2bead = [ld('100K/2b', 1), ld('150K/2b', 1), ld('200K/2b', 1), ld('250K/2b', 1), 
-                ld('300K/2b', 1), ld('350K/2b', 1), ld('400K/2b', 1), ld('500K/2b', 1)]
-
-
-volumes_4bead = [ld('100K/4b', 1), ld('150K/4b', 1), ld('200K/4b', 1), ld('250K/4b', 1), 
-                ld('300K/4b', 1), ld('350K/4b', 1), ld('400K/4b', 1), ld('500K/4b', 1)]
+volumes_2bead = [ld('100K/100K_2b', 1), ld('150K/150K_2b', 1), ld('200K/200K_2b', 1), ld('250K/250K_2b', 1), 
+                ld('300K/300K_2b', 1), ld('350K/350K_2b', 1), ld('400K/400K_2b', 1), ld('500K/500K_2b', 1)]
 
 
-volumes_8bead = [ld('100K/8b', 2), ld('150K/8b', 2), ld('200K/8b', 2), ld('250K/8b', 2), 
-                ld('300K/8b', 2), ld('350K/8b', 2), ld('400K/8b', 2), ld('500K/8b', 2)]
+volumes_4bead = [ld('100K/100K_4b', 1), ld('150K/150K_4b', 1), ld('200K/200K_4b', 1), ld('250K/250K_4b', 1), 
+                ld('300K/300K_4b', 1), ld('350K/350K_4b', 1), ld('400K/400K_4b', 1), ld('500K/500K_4b', 1)]
 
 
-volumes_16bead = [ld('100K/16b', 3), ld('150K/16b', 3), ld('200K/16b', 3), ld('250K/16b', 3), 
-                ld('300K/16b', 3), ld('350K/16b', 3), ld('400K/16b', 3), ld('500K/16b', 3)]
+volumes_8bead = [ld('100K/100K_8b', 2), ld('150K/150K_8b', 2), ld('200K/200K_8b', 2), ld('250K/250K_8b', 2), 
+                ld('300K/300K_8b', 2), ld('350K/350K_8b', 2), ld('400K/400K_8b', 2), ld('500K/500K_8b', 2)]
 
 
-volumes_32bead = [ld('100K/32b', 3), ld('150K/32b', 3), ld('200K/32b', 3), ld('250K/32b', 3), 
-                ld('300K/32b', 3), ld('350K/32b', 3), ld('400K/32b', 2), ld('500K/32b', 2)]
+volumes_16bead = [ld('100K/100K_16b', 3), ld('150K/150K_16b', 3), ld('200K/200K_16b', 3), ld('250K/250K_16b', 3), 
+                ld('300K/300K_16b', 3), ld('350K/350K_16b', 3), ld('400K/400K_16b', 3), ld('500K/500K_16b', 3)]
 
 
-volumes_64bead = [ld('100K/64b', 2), ld('150K/64b', 2), ld('200K/64b', 2), ld('250K/64b', 1), 
-                ld('300K/64b', 1), ld('350K/64b', 2)]
+volumes_32bead = [ld('100K/100K_32b', 3), ld('150K/150K_32b', 3), ld('200K/200K_32b', 3), ld('250K/250K_32b', 3), 
+                ld('300K/300K_32b', 3), ld('350K/350K_32b', 3), ld('400K/400K_32b', 2), ld('500K/500K_32b', 2)]
 
 
-volumes_128bead = [ld('100K/128b', 2), ld('150K/128b', 2), ld('200K/128b', 2), ld('250K/128b', 2)]
+volumes_64bead = [ld('100K/100K_64b', 2), ld('150K/150K_64b', 2), ld('200K/200K_64b', 2), ld('250K/250K_64b', 1), 
+                ld('300K/300K_64b', 1), ld('350K/350K_64b', 2), ld('400K/400K_64b', 2), ld('500K/500K_64b', 2)]
+
+
+volumes_128bead = [ld('100K/100K_128b', 2), ld('150K/150K_128b', 2)]
 
 # Calculate staistics of the data
 vol_1bead, std_1bead, sem_1bead = calc_stats(volumes_1bead)
@@ -100,10 +100,10 @@ ax.scatter(temps, vol_16bead, color='#00439c', alpha=0.65, s=22)
 err5 = ax.errorbar(temps, vol_16bead, label="P=16", yerr=sem_16bead, capsize=2.0, fmt="o", color='#00439c', markersize=6, marker="P", linestyle=(0, (5, 1)))
 ax.scatter(temps, vol_32bead, color='purple', alpha=0.65, s=22)
 err6 = ax.errorbar(temps, vol_32bead, label="P=32", yerr=sem_32bead, capsize=2.0, fmt="o", color='purple', markersize=6, marker="*", linestyle=(5, (10, 3)))
-ax.scatter(temps[0:6], vol_64bead, color='#520202', alpha=0.65, s=22)
-err7 = ax.errorbar(temps[0:6], vol_64bead, label="P=64", yerr=sem_64bead, capsize=2.0, fmt="o", color='#520202', markersize=6, marker="p", linestyle=(0, (3, 1, 1, 1)))
-ax.scatter(temps[0:4], vol_128bead, color='orange', alpha=0.65, s=22)
-err8 = ax.errorbar(temps[0:4], vol_128bead, label="P=128", yerr=sem_128bead, capsize=2.0, fmt="o", color='orange', markersize=6, marker="x", linestyle=(0, (3, 1, 1, 1, 1, 1)))
+ax.scatter(temps, vol_64bead, color='#520202', alpha=0.65, s=22)
+err7 = ax.errorbar(temps, vol_64bead, label="P=64", yerr=sem_64bead, capsize=2.0, fmt="o", color='#520202', markersize=6, marker="p", linestyle=(0, (3, 1, 1, 1)))
+ax.scatter(temps[0:2], vol_128bead, color='orange', alpha=0.65, s=22)
+err8 = ax.errorbar(temps[0:2], vol_128bead, label="P=128", yerr=sem_128bead, capsize=2.0, fmt="o", color='orange', markersize=6, marker="x", linestyle=(0, (3, 1, 1, 1, 1, 1)))
 ax.set_xlabel('Temperature (K)', fontsize=14)
 ax.set_ylabel('Volume (Å$^{3}$)', fontsize=14)
 ax.set_ylim(182.25, 184.75)
@@ -129,15 +129,15 @@ axins2.scatter(temps, vol_16bead, color='#00439c', alpha=0.65, s=22)
 err5 = axins2.errorbar(temps, vol_16bead, label="P=16", yerr=sem_16bead, capsize=2.0, fmt="o", color='#00439c', markersize=6, marker="P", linestyle=(0, (5, 1)))
 axins2.scatter(temps, vol_32bead, color='purple', alpha=0.65, s=22)
 err6 = axins2.errorbar(temps, vol_32bead, label="P=32", yerr=sem_32bead, capsize=2.0, fmt="o", color='purple', markersize=6, marker="*", linestyle=(5, (10, 3)))
-axins2.scatter(temps[0:6], vol_64bead, color='#520202', alpha=0.65, s=22)
-err7 = axins2.errorbar(temps[0:6], vol_64bead, label="P=64", yerr=sem_64bead, capsize=2.0, fmt="o", color='#520202', markersize=6, marker="p", linestyle=(0, (3, 1, 1, 1)))
-axins2.scatter(temps[0:4], vol_128bead, color='orange', alpha=0.65, s=22)
-err8 = axins2.errorbar(temps[0:4], vol_128bead, label="P=128", yerr=sem_128bead, capsize=2.0, fmt="o", color='orange', markersize=6, marker="x", linestyle=(0, (3, 1, 1, 1, 1, 1)))
+axins2.scatter(temps, vol_64bead, color='#520202', alpha=0.65, s=22)
+err7 = axins2.errorbar(temps, vol_64bead, label="P=64", yerr=sem_64bead, capsize=2.0, fmt="o", color='#520202', markersize=6, marker="p", linestyle=(0, (3, 1, 1, 1)))
+axins2.scatter(temps[0:2], vol_128bead, color='orange', alpha=0.65, s=22)
+err8 = axins2.errorbar(temps[0:2], vol_128bead, label="P=128", yerr=sem_128bead, capsize=2.0, fmt="o", color='orange', markersize=6, marker="x", linestyle=(0, (3, 1, 1, 1, 1, 1)))
 axins2.set_xlim(90, 510)
 axins2.set_ylim(184.575, 184.725)
 axins2.set_xticks([100, 200, 300, 400, 500])
 axins2.set_yticks([184.600, 184.650, 184.700])
 axins2.tick_params(axis='both', labelsize=11, which='both', bottom=True, left=True, zorder=True)
 
-plt.savefig('figure_6.png', dpi=300, transparent=True)
+plt.savefig('myfig.png', dpi=300, transparent=True)
 plt.show()
