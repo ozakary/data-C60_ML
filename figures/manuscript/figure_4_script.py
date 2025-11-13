740f449c8198f3df9222d73a7126131bb5b86395
ossi0 <ossi.laurila000@gmail.com>
2025-11-13 15:37:13 +0200

    Add files via upload

---
+++ figures/manuscript/figure_4_script.py
@@ -0,0 +1,151 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
+
+fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3, layout='none', figsize=(14, 7))
+
+fig.subplots_adjust(left=0.074, right=0.945, wspace=0.257, top=0.954, bottom=0.082, hspace=0.244)
+
+# Load energy results of MLIP-1
+energy_test_m1 = np.loadtxt('log/MLIP-1/energy_test.out')
+energy_train_m1 = np.loadtxt('log/MLIP-1/energy_train.out')
+
+# Load energy results of MLIP-2
+energy_test_m2 = np.loadtxt('log/MLIP-2/energy_test.out')
+energy_train_m2 = np.loadtxt('log/MLIP-2/energy_train.out')
+
+# Load energy results of MLIP-3
+energy_test_m3 = np.loadtxt('log/MLIP-3/energy_test.out')
+energy_train_m3 = np.loadtxt('log/MLIP-3/energy_train.out')
+
+# Load force results of MLIP-1
+force_test_m1 = np.loadtxt('log/MLIP-1/force_test.out')
+force_train_m1 = np.loadtxt('log/MLIP-1/force_train.out')
+
+# Load force results of MLIP-2
+force_test_m2 = np.loadtxt('log/MLIP-2/force_test.out')
+force_train_m2 = np.loadtxt('log/MLIP-2/force_train.out')
+
+# Load force results of MLIP-3
+force_test_m3 = np.loadtxt('log/MLIP-3/force_test.out')
+force_train_m3 = np.loadtxt('log/MLIP-3/force_train.out')
+
+# Extract the data of predicted force components
+force_test_comb_m1 = np.concatenate((force_test_m1[:, 0], force_test_m1[:, 1], force_test_m1[:, 2]), axis=None)
+force_train_comb_m1 = np.concatenate((force_train_m1[:, 0], force_train_m1[:, 1], force_train_m1[:, 2]), axis=None)
+
+force_test_comb_m2 = np.concatenate((force_test_m2[:, 0], force_test_m2[:, 1], force_test_m2[:, 2]), axis=None)
+force_train_comb_m2 = np.concatenate((force_train_m2[:, 0], force_train_m2[:, 1], force_train_m2[:, 2]), axis=None)
+
+force_test_comb_m3 = np.concatenate((force_test_m3[:, 0], force_test_m3[:, 1], force_test_m3[:, 2]), axis=None)
+force_train_comb_m3 = np.concatenate((force_train_m3[:, 0], force_train_m3[:, 1], force_train_m3[:, 2]), axis=None)
+
+
+# Extract the data of DFT force components
+force_test_comb_m1_true = np.concatenate((force_test_m1[:, 3], force_test_m1[:, 4], force_test_m1[:, 5]), axis=None)
+force_train_comb_m1_true = np.concatenate((force_train_m1[:, 3], force_train_m1[:, 4], force_train_m1[:, 5]), axis=None)
+
+force_test_comb_m2_true = np.concatenate((force_test_m2[:, 3], force_test_m2[:, 4], force_test_m2[:, 5]), axis=None)
+force_train_comb_m2_true = np.concatenate((force_train_m2[:, 3], force_train_m2[:, 4], force_train_m2[:, 5]), axis=None)
+
+force_test_comb_m3_true = np.concatenate((force_test_m3[:, 3], force_test_m3[:, 4], force_test_m3[:, 5]), axis=None)
+force_train_comb_m3_true = np.concatenate((force_train_m3[:, 3], force_train_m3[:, 4], force_train_m3[:, 5]), axis=None)
+
+#MLIP-1 energy correlation plot
+slope_e_test_m1, intercept_e_test_m1, r_value_e_test_m1, p_value_e_test_m1, std_err_e_test_m1 = stats.linregress(energy_test_m1[:, 1], energy_test_m1[:, 0])
+ax1.scatter(60*energy_train_m1[:, 1], 60*energy_train_m1[:, 0], marker='o', alpha=0.4, color='blue', label="Train")
+ax1.scatter(60*energy_test_m1[:, 1], 60*energy_test_m1[:, 0], marker= 'x', alpha=0.4, color='darkorange', label="Test")
+ax1.plot(np.linspace(-1.489*60, -1.425*60), np.linspace(-1.489*60, -1.425*60), 'r-', alpha=0.8)
+ax1.set_xlabel(r'$E_{\text{DFT}}$ (eV)', fontsize=13)
+ax1.set_ylabel(r'$E_{\text{ML}}$ (eV)', fontsize=13, x=-2.0)
+ax1.set_xticks([-1.48*60, -1.46*60, -1.44*60])
+ax1.set_yticks([-1.48*60, -1.46*60, -1.44*60])
+ax1.text(.04, .91, 'train. RMSE = 11.4 meV', fontsize=12, transform=ax1.transAxes)
+ax1.text(.04, .83, 'valid. RMSE = 11.4 meV', fontsize=12, transform=ax1.transAxes)
+ax1.text(.04, .75, f'valid. R$^2$ = {r_value_e_test_m1**2:.4f}', fontsize=12, transform=ax1.transAxes)
+ax1.tick_params(axis='x', labelsize=13)
+ax1.tick_params(axis='y', labelsize=13)
+ax1.legend(["train.", "valid."], loc="lower right", fontsize=13, framealpha=1)
+ax1.set_title("MLIP-1.2", fontsize=13)
+ax1.set_xlim([-1.489*60, -1.425*60])
+ax1.set_ylim([-1.489*60, -1.425*60])
+
+#MLIP-2 energy correlation plot
+slope_e_test_m2, intercept_e_test_m2, r_value_e_test_m2, p_value_e_test_m2, std_err_e_test_m2 = stats.linregress(energy_test_m2[:, 1], energy_test_m2[:, 0])
+ax2.scatter(60*energy_train_m2[:, 1], 60*energy_train_m2[:, 0], marker = 'o', alpha=0.4, color='blue')
+ax2.scatter(60*energy_test_m2[:, 1], 60*energy_test_m2[:, 0], marker = 'x', alpha=0.4, color='darkorange')
+ax2.plot(np.linspace(-1.4*60, -1.335*60), np.linspace(-1.4*60, -1.335*60), 'r-', alpha=0.8)
+ax2.set_xlabel(r'$E_{\text{DFT}}$ (eV)', fontsize=13)
+ax2.set_xticks([-1.39*60, -1.37*60, -1.35*60])
+ax2.set_yticks([-1.39*60, -1.37*60, -1.35*60])
+ax2.text(.04, .91, 'train. RMSE = 12.0 meV', fontsize=12, transform=ax2.transAxes)
+ax2.text(.04, .83, 'valid. RMSE = 12.0 meV', fontsize=12, transform=ax2.transAxes)
+ax2.text(.04, .75, f'valid. R$^2$ = {r_value_e_test_m2**2:.4f}', fontsize=12, transform=ax2.transAxes)
+ax2.tick_params(axis='x', labelsize=13)
+ax2.tick_params(axis='y', labelsize=13)
+ax2.set_title("MLIP-2", fontsize=13)
+ax2.set_xlim([-1.4*60, -1.335*60])
+ax2.set_ylim([-1.4*60, -1.335*60])
+
+#MLIP-3 energy correlation plot
+slope_e_test_m3, intercept_e_test_m3, r_value_e_test_m3, p_value_e_test_m3, std_err_e_test_m3 = stats.linregress(energy_test_m3[:, 1], energy_test_m3[:, 0])
+ax3.scatter(60*energy_train_m3[:, 1], 60*energy_train_m3[:, 0], marker = 'o', alpha=0.4, color='blue')
+ax3.scatter(60*energy_test_m3[:, 1], 60*energy_test_m3[:, 0], marker = 'x', alpha=0.4, color='darkorange')
+ax3.plot(np.linspace(-0.503*60, -0.437*60), np.linspace(-0.503*60, -0.437*60), 'r-', alpha=0.8)
+ax3.set_xlabel(r'$E_{\text{DFT}}$ (eV)', fontsize=13)
+ax3.set_xticks([-0.5*60, -0.48*60, -0.46*60, -0.44*60])
+ax3.set_yticks([-0.5*60, -0.48*60, -0.46*60, -0.44*60])
+ax3.text(.04, .91, 'train. RMSE = 10.8 meV', fontsize=12, transform=ax3.transAxes)
+ax3.text(.04, .83, 'valid. RMSE = 13.2 meV', fontsize=12, transform=ax3.transAxes)
+ax3.text(.04, .75, f'valid. R$^2$ = {r_value_e_test_m3**2:.4f}', fontsize=12, transform=ax3.transAxes)
+ax3.tick_params(axis='x', labelsize=13)
+ax3.tick_params(axis='y', labelsize=13)
+ax3.set_title("MLIP-3", fontsize=13)
+ax3.set_xlim([-0.503*60, -0.437*60])
+ax3.set_ylim([-0.503*60, -0.437*60])
+
+#MLIP-1 force correlation plot
+slope_f_test_m1, intercept_f_test_m1, r_value_f_test_m1, p_value_f_test_m1, std_err_f_test_m1 = stats.linregress(force_test_comb_m1_true, force_test_comb_m1)
+ax4.scatter(force_train_m1[:, 3:6], force_train_m1[:, 0:3], marker = 'o', alpha=0.4, color='blue', label="Train")
+ax4.scatter(force_test_m1[:, 3:6], force_test_m1[:, 0:3], marker = 'x', alpha=0.4, color='darkorange', label="Test")
+ax4.plot(np.linspace(-10,10), np.linspace(-10,10), 'r-', alpha=0.8)
+ax4.set_xlabel(r'$\vec f_{\text{DFT}}$ (eV/Å)', fontsize=13)
+ax4.set_ylabel(r'$\vec f_{\text{ML}}$ (eV/Å)', fontsize=13, x=-2.0)
+ax4.text(0.05, 0.91, 'train. RMSE = 0.0432 eV/Å', fontsize=12, transform=ax4.transAxes)
+ax4.text(0.05, 0.83, 'valid. RMSE = 0.0435 eV/Å', fontsize=12, transform=ax4.transAxes)
+ax4.text(0.05, 0.75, f'valid. R$^2$ = {r_value_f_test_m1**2:.4f}', fontsize=12, transform=ax4.transAxes)
+ax4.tick_params(axis='x', labelsize=13)
+ax4.tick_params(axis='y', labelsize=13)
+ax4.set_xlim([-10, 10])
+ax4.set_ylim([-10, 10])
+
+#MLIP-2 force correlation plot
+slope_f_test_m2, intercept_f_test_m2, r_value_f_test_m2, p_value_f_test_m2, std_err_f_test_m2 = stats.linregress(force_test_comb_m2_true, force_test_comb_m2)
+ax5.scatter(force_train_m2[:, 3:6], force_train_m2[:, 0:3], marker = 'o', alpha=0.4, color='blue')
+ax5.scatter(force_test_m2[:, 3:6], force_test_m2[:, 0:3], marker = 'x', alpha=0.4, color='darkorange')
+ax5.plot(np.linspace(-10,10), np.linspace(-10,10), 'r-', alpha=0.8)
+ax5.set_xlabel(r'$\vec f_{\text{DFT}}$ (eV/Å)', fontsize=13)
+ax5.text(0.05, 0.90, 'train. RMSE = 0.0430 eV/Å', fontsize=12, transform=ax5.transAxes)
+ax5.text(0.05, 0.82, 'valid. RMSE = 0.0448 eV/Å', fontsize=12, transform=ax5.transAxes)
+ax5.text(0.05, 0.74, f'valid. R$^2$ = {r_value_f_test_m2**2:.4f}', fontsize=12, transform=ax5.transAxes)
+ax5.tick_params(axis='x', labelsize=13)
+ax5.tick_params(axis='y', labelsize=13)
+ax5.set_xlim([-10, 10])
+ax5.set_ylim([-10, 10])
+
+#MLIP-3 force correlation plot
+slope_f_test_m3, intercept_f_test_m3, r_value_f_test_m3, p_value_f_test_m3, std_err_f_test_m3 = stats.linregress(force_test_comb_m3_true, force_test_comb_m3)
+ax6.scatter(force_train_m3[:, 3:6], force_train_m3[:, 0:3], marker = 'o', alpha=0.4, color='blue')
+ax6.scatter(force_test_m3[:, 3:6], force_test_m3[:, 0:3], marker = 'x', alpha=0.4, color='darkorange')
+ax6.plot(np.linspace(-10,10), np.linspace(-10,10), 'r-', alpha=0.8)
+ax6.set_xlabel(r'$\vec f_{\text{DFT}}$ (eV/Å)', fontsize=13)
+ax6.text(0.05, 0.90, 'train. RMSE = 0.0452 eV/Å', fontsize=12, transform=ax6.transAxes)
+ax6.text(0.05, 0.82, 'valid. RMSE = 0.0463 eV/Å', fontsize=12, transform=ax6.transAxes)
+ax6.text(0.05, 0.74, f'valid. R$^2$ = {r_value_f_test_m3**2:.4f}', fontsize=12, transform=ax6.transAxes)
+ax6.tick_params(axis='x', labelsize=13)
+ax6.tick_params(axis='y', labelsize=13)
+ax6.set_xlim([-10, 10])
+ax6.set_ylim([-10, 10])
+
+plt.savefig('figure_4.png', dpi=300)
+plt.show()
---
+++ figures/manuscript/figure_6_script.py
@@ -0,0 +1,141 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+linestyle_tuple = [
+     ('loosely dotted',        (0, (1, 10))),
+     ('dotted',                (0, (1, 5))),
+     ('densely dotted',        (0, (1, 1))),
+
+     ('long dash with offset', (5, (10, 3))),
+     ('loosely dashed',        (0, (5, 10))),
+     ('dashed',                (0, (5, 5))),
+     ('densely dashed',        (0, (5, 1))),
+
+     ('loosely dashdotted',    (0, (3, 10, 1, 10))),
+     ('dashdotted',            (0, (3, 5, 1, 5))),
+     ('densely dashdotted',    (0, (3, 1, 1, 1))),
+
+     ('dashdotdotted',         (0, (3, 5, 1, 5, 1, 5))),
+     ('loosely dashdotdotted', (0, (3, 10, 1, 10, 1, 10))),
+     ('densely dashdotdotted', (0, (3, 1, 1, 1, 1, 1)))]
+
+
+#Load data of simulation in folder f and split the data at interval s
+def ld(f):
+    try:
+        return np.concatenate(np.loadtxt('./convergence/' + f + '/allvol.txt'), axis=None)
+    except FileNotFoundError:
+        return np.concatenate(np.loadtxt('./convergence/' + f + '/volumes_ell_average.txt'), axis=None)
+
+#Calculate mean and stdev and standard error
+def calc_stats(data):
+    means = []
+    devs = []
+    sems = []
+    for el in data:
+        means.append(np.round(np.mean(el), 3))
+        devs.append(np.round(np.std(el), 3))
+        sems.append(np.round(np.std(el)/(len(el))**(1/2), 4))
+    return means, devs, sems
+
+# Load the data
+volumes_1bead = [ld('100K/100K_1b'), ld('150K/150K_1b'), ld('200K/200K_1b'), ld('250K/250K_1b'), 
+                ld('300K/300K_1b'), ld('350K/350K_1b'), ld('400K/400K_1b'), ld('500K/500K_1b')]
+
+volumes_2bead = [ld('100K/100K_2b'), ld('150K/150K_2b'), ld('200K/200K_2b'), ld('250K/250K_2b'), 
+                ld('300K/300K_2b'), ld('350K/350K_2b'), ld('400K/400K_2b'), ld('500K/500K_2b')]
+
+
+volumes_4bead = [ld('100K/100K_4b'), ld('150K/150K_4b'), ld('200K/200K_4b'), ld('250K/250K_4b'), 
+                ld('300K/300K_4b'), ld('350K/350K_4b'), ld('400K/400K_4b'), ld('500K/500K_4b')]
+
+
+volumes_8bead = [ld('100K/100K_8b'), ld('150K/150K_8b'), ld('200K/200K_8b'), ld('250K/250K_8b'), 
+                ld('300K/300K_8b'), ld('350K/350K_8b'), ld('400K/400K_8b'), ld('500K/500K_8b')]
+
+
+volumes_16bead = [ld('100K/100K_16b'), ld('150K/150K_16b'), ld('200K/200K_16b'), ld('250K/250K_16b'), 
+                ld('300K/300K_16b'), ld('350K/350K_16b'), ld('400K/400K_16b'), ld('500K/500K_16b')]
+
+
+volumes_32bead = [ld('100K/100K_32b'), ld('150K/150K_32b'), ld('200K/200K_32b'), ld('250K/250K_32b'), 
+                ld('300K/300K_32b'), ld('350K/350K_32b'), ld('400K/400K_32b'), ld('500K/500K_32b')]
+
+
+volumes_64bead = [ld('100K/100K_64b'), ld('150K/150K_64b'), ld('200K/200K_64b'), ld('250K/250K_64b'), 
+                ld('300K/300K_64b'), ld('350K/350K_64b')]
+
+
+volumes_128bead = [ld('100K/100K_128b'), ld('150K/150K_128b'), ld('200K/200K_128b'), ld('250K/250K_128b')]
+# Calculate staistics of the data
+vol_1bead, std_1bead, sem_1bead = calc_stats(volumes_1bead)
+vol_2bead, std_2bead, sem_2bead = calc_stats(volumes_2bead)
+vol_4bead, std_4bead, sem_4bead = calc_stats(volumes_4bead)
+vol_8bead, std_8bead, sem_8bead = calc_stats(volumes_8bead)
+vol_16bead, std_16bead, sem_16bead = calc_stats(volumes_16bead)
+vol_32bead, std_32bead, sem_32bead = calc_stats(volumes_32bead)
+vol_64bead, std_64bead, sem_64bead = calc_stats(volumes_64bead)
+vol_128bead, std_128bead, sem_128bead = calc_stats(volumes_128bead)
+
+# x-axis
+temps = [100, 150, 200, 250, 300, 350, 400, 500]
+
+# Plot the data
+fig, ax = plt.subplots(figsize=(8, 7))
+fig.subplots_adjust(left=0.144, right=0.943, wspace=0.533, top=0.843, bottom=0.099)
+
+plt.rcParams.update({'font.size': 16})
+
+ax.scatter(temps, vol_1bead, color='green', alpha=0.65, s=22)
+err1 = ax.errorbar(temps, vol_1bead, label="$P$=1", yerr=sem_1bead, capsize=2.0, fmt="o", color='green', markersize=6, marker="o", linestyle="solid")
+ax.scatter(temps, vol_2bead, color='#ba9500', alpha=0.65, s=22)
+err2 = ax.errorbar(temps, vol_2bead, label="$P$=2", yerr=sem_2bead, capsize=2.0, fmt="o", color='#ba9500', markersize=6, marker="v", linestyle="dotted")
+ax.scatter(temps, vol_4bead, color='#2dad76', alpha=0.65, s=22)
+err3 = ax.errorbar(temps, vol_4bead, label="$P$=4", yerr=sem_4bead, capsize=2.0, fmt="o", color='#2dad76', markersize=6, marker="d", linestyle="dashed")
+ax.scatter(temps, vol_8bead, color='blue', alpha=0.65, s=22)
+err4 = ax.errorbar(temps, vol_8bead, label="$P$=8", yerr=sem_8bead, capsize=2.0, fmt="o", color='blue', markersize=6, marker="s", linestyle="dashdot")
+ax.scatter(temps, vol_16bead, color='#00439c', alpha=0.65, s=22)
+err5 = ax.errorbar(temps, vol_16bead, label="$P$=16", yerr=sem_16bead, capsize=2.0, fmt="o", color='#00439c', markersize=6, marker="P", linestyle=(0, (5, 1)))
+ax.scatter(temps, vol_32bead, color='purple', alpha=0.65, s=22)
+err6 = ax.errorbar(temps, vol_32bead, label="$P$=32", yerr=sem_32bead, capsize=2.0, fmt="o", color='purple', markersize=6, marker="*", linestyle=(5, (10, 3)))
+ax.scatter(temps[0:6], vol_64bead, color='#520202', alpha=0.65, s=22)
+err7 = ax.errorbar(temps[0:6], vol_64bead, label="$P$=64", yerr=sem_64bead, capsize=2.0, fmt="o", color='#520202', markersize=6, marker="p", linestyle=(0, (3, 1, 1, 1)))
+ax.scatter(temps[0:4], vol_128bead, color='orange', alpha=0.65, s=22)
+err8 = ax.errorbar(temps[0:4], vol_128bead, label="$P$=128", yerr=sem_128bead, capsize=2.0, fmt="o", color='orange', markersize=6, marker="x", linestyle=(0, (3, 1, 1, 1, 1, 1)))
+ax.set_xlabel('$T$ (K)', fontsize=14)
+ax.set_ylabel(r'$\langle V_{\text{eff}} \rangle\ (Å^{3}$)', fontsize=14)
+ax.set_ylim(182.25, 184.75)
+ax.tick_params(axis='x', labelsize=14)
+ax.tick_params(axis='y', labelsize=14)
+ax.legend(handles=[err8, err7, err6, err5, err4, err3, err2, err1], loc="upper center", fontsize=14, ncols=4, bbox_to_anchor=(0.5, 1.2))
+ax.grid(True)
+
+# inset Axes....
+x1, x2, y1, y2 = 90, 510, 184.55, 184.665  # subregion of the original image
+axins2 = ax.inset_axes(
+    [0.41, 0.05, 0.57, 0.37],
+    xlim=(x1, x2), ylim=(y1, y2))
+axins2.scatter(temps, vol_1bead, color='green', alpha=0.65, s=22)
+err1 = axins2.errorbar(temps, vol_1bead, label="$P$=1", yerr=sem_1bead, capsize=2.0, fmt="o", color='green', markersize=6, marker="o", linestyle="solid")
+axins2.scatter(temps, vol_2bead, color='#ba9500', alpha=0.65, s=22)
+err2 = axins2.errorbar(temps, vol_2bead, label="$P$=2", yerr=sem_2bead, capsize=2.0, fmt="o", color='#ba9500', markersize=6, marker="v", linestyle="dotted")
+axins2.scatter(temps, vol_4bead, color='#2dad76', alpha=0.65, s=22)
+err3 = axins2.errorbar(temps, vol_4bead, label="$P$=4", yerr=sem_4bead, capsize=2.0, fmt="o", color='#2dad76', markersize=6, marker="d", linestyle="dashed")
+axins2.scatter(temps, vol_8bead, color='blue', alpha=0.65, s=22)
+err4 = axins2.errorbar(temps, vol_8bead, label="$P$=8", yerr=sem_8bead, capsize=2.0, fmt="o", color='blue', markersize=6, marker="s", linestyle="dashdot")
+axins2.scatter(temps, vol_16bead, color='#00439c', alpha=0.65, s=22)
+err5 = axins2.errorbar(temps, vol_16bead, label="$P$=16", yerr=sem_16bead, capsize=2.0, fmt="o", color='#00439c', markersize=6, marker="P", linestyle=(0, (5, 1)))
+axins2.scatter(temps, vol_32bead, color='purple', alpha=0.65, s=22)
+err6 = axins2.errorbar(temps, vol_32bead, label="$P$=32", yerr=sem_32bead, capsize=2.0, fmt="o", color='purple', markersize=6, marker="*", linestyle=(5, (10, 3)))
+axins2.scatter(temps[0:6], vol_64bead, color='#520202', alpha=0.65, s=22)
+err7 = axins2.errorbar(temps[0:6], vol_64bead, label="$P$=64", yerr=sem_64bead, capsize=2.0, fmt="o", color='#520202', markersize=6, marker="p", linestyle=(0, (3, 1, 1, 1)))
+axins2.scatter(temps[0:4], vol_128bead, color='orange', alpha=0.65, s=22)
+err8 = axins2.errorbar(temps[0:4], vol_128bead, label="$P$=128", yerr=sem_128bead, capsize=2.0, fmt="o", color='orange', markersize=6, marker="x", linestyle=(0, (3, 1, 1, 1, 1, 1)))
+axins2.set_xlim(90, 510)
+axins2.set_ylim(184.55, 184.665)
+axins2.set_xticks([100, 200, 300, 400, 500])
+axins2.set_yticks([184.55, 184.60, 184.650])
+axins2.tick_params(axis='both', labelsize=13, which='both', bottom=True, left=True, zorder=True)
+
+plt.savefig('figure_6.png', dpi=300)
+plt.show()
